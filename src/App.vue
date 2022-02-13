9c493b93041f34dcf548d844003e97ec5f7e6a85
Augusto <augustodiaz_1@hotmail.com>
2022-02-13 15:35:36 +1000

    Todo-app

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+  max-width: 960px;
+  margin: 0 auto;
+  margin-top: 8rem;
+  background-color: #333;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  border: 4px solid #42b983;
+}
+#nav a {
+  font-weight: bold;
+  color: #fff;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos :todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "../components/Todos.vue";
+import AddTodo from "../components/AddTodo.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: { Todos, AddTodo },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(
+          (res) => (this.todos = this.todos.filter((todo) => todo.id !== id))
+        )
+        .catch((err) => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+</style>
